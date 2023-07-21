e98017354a7cc582c12d35dfa0d2689e82408cd3
lukketsvane <iverfinne@gmail.com>
2023-07-21 20:43:52 +0200

    Initial Commit

---
+++ client/web/src/components/Footer/index.jsx
@@ -0,0 +1,19 @@
+/**
+ * src/components/Footer/index.jsx
+ * Footer message and public links
+ * 
+ * created by Lynchee on 7/16/23
+ */
+
+import React from 'react';
+import { FaGithub, FaDiscord, FaTwitter } from 'react-icons/fa';
+import './style.css';
+
+const Footer = () => (
+    <footer>
+    
+    </footer>
+);
+
+export default Footer;
+
